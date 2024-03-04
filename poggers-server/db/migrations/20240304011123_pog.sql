5b79b38d259875c24d0c9925ef147a36e86b4bea
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2024-03-04 09:54:06 +0800

    added: pogs table db

--- poggers-server/db/migrations/20240304011123_pog.sql
+++ poggers-server/db/migrations/20240304011123_pog.sql
@@ -1,4 +1,10 @@
 -- migrate:up
-
+CREATE TABLE pogs (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    ticker_symbol VARCHAR(255) NOT NULL,
+    color VARCHAR(255) NOT NULL
+);
 
 -- migrate:down
+drop table pogs
