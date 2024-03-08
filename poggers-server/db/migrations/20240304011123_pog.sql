6d7ca97c83c02c6018151d4e7e32de359356a8d3
chdean-09 <chaddenard.andrada-19@cpu.edu.ph>
2024-03-09 00:43:52 +0800

    edit db to have unique constraints

--- poggers-server/db/migrations/20240304011123_pog.sql
+++ poggers-server/db/migrations/20240304011123_pog.sql
@@ -1,8 +1,8 @@
 -- migrate:up
 CREATE TABLE pogs (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    ticker_symbol VARCHAR(255) NOT NULL,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    ticker_symbol VARCHAR(255) UNIQUE NOT NULL,
     color VARCHAR(255) NOT NULL,
     price DECIMAL NOT NULL
 );
